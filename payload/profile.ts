0d7ed35f2e033bc2e91f1f9825a2b20c1c6dd8f5
seunghyunbaek <horowitz@hanmail.net>
2024-10-21 21:54:46 +0900

    [feat] 기술블로그 링크 추가
    - 추가

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,5 +1,5 @@
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faBlog, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { IProfile } from '../component/profile/IProfile';
@@ -30,13 +30,13 @@ const profile: IProfile.Payload = {
       icon: faGithub,
     },
     {
+      link: 'https://velog.io/@littlegiant/posts',
+      icon: faBlog,
+    },
+    {
       title: 'https://www.linkedin.com/in/승현-백-80ab96226/',
       link: 'https://www.linkedin.com/in/%EC%8A%B9%ED%98%84-%EB%B0%B1-80ab96226/',
       icon: faLinkedin,
-    },
-    {
-      link: 'https://www.instagram.com/l_giant_b/',
-      icon: faInstagram,
     },
   ],
   notice: {
